cbc0c5c8b7f3d608d65e91d77aa20031a99760b8
Vishnu-54 <www.vishnu2002129139@gmail.com>
2026-04-15 23:17:30 +0530

    Remove node_modules from repo and fix Netlify build

--- credwise-ai/backend/src/main/java/com/credwiseai/credwiseai/config/CorsConfig.java
+++ credwise-ai/backend/src/main/java/com/credwiseai/credwiseai/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(
-                List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://aicred-2.onrender.com"));
+                List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://aicred-4.onrender.com/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
